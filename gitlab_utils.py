a41bb7fcebb6d7cadef509e8d3e21ca6cf37b5c0
Aljoša Mrak <aljosa.mrak@outfit7.com>
2019-08-03 17:35:10 +0200

    Add caching for gitlab files

--- gitlab_utils.py
+++ gitlab_utils.py
@@ -3,17 +3,27 @@ from gitlab import GitlabGetError
 
 
 def grep_from_file(project, file_path, revision, regex_pattern):
-    try:
-        _file = project.files.get(file_path=file_path, ref=revision)
-    except GitlabGetError:
+    content = _get_file(project, file_path, revision)
+    if content is None:
         return None
-
-    if _file is None:
-        return None
-    content = _file.decode()
 
     matches = re.search(regex_pattern, content)
-    if matches is None:
-        return None
+    return None if matches is None else matches.group(1)
 
-    return matches.group(1)
+cache = dict()
+
+def _get_file(project, file_path, revision):
+    key = (project, revision, file_path)
+    if key in cache:
+        content = cache[key]
+    else:
+        try:
+            _file = project.files.get(file_path=file_path, ref=revision)
+        except GitlabGetError:
+            return None
+
+        if _file is None:
+            return None
+        content = _file.decode().decode('utf-8')
+        cache[key] = content
+    return content
